3520aa9e4af496dda9e26a80e75506e042c9c1d7
gsPatrick <patrickgsiqueira@hotmail.com>
2025-01-25 15:29:53 -0300

    Cors no sistema

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 6000);
+
+    const logger = new Logger('Bootstrap');
+
+  // Configuração do CORS
+  app.enableCors({
+    origin: '*', // Ou uma lista de origens permitidas
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+  });
+
+    const port = process.env.PORT || 6000
+    await app.listen(port);
+    logger.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap();
